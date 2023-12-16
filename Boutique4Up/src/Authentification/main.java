48e19e5ddcd4f46b29c001d91ca105ffd014d44b
zakaria kerroumi <za.kerroumi42@gmail.com>
2023-12-16 09:43:58 +0100

    make authentification :login and signUp

---
+++ Boutique4Up/src/Authentification/main.java
@@ -0,0 +1,15 @@
+
+package Authentification;
+
+public class main {
+
+
+    public static void main(String[] args) {
+
+        Login LoginFrame = new Login();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null); 
+    }
+    
+}
